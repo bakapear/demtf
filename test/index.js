2e2783ec8f0b26f2bfce262aa0d889dd30416def
bakapear <proxybomb@gmail.com>
2023-09-10 14:09:22 +0200

    compare decode stream with encode stream in test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 let DemTF = require('../')
-let demo = new DemTF('./test/demo.dem')
+let demo = new DemTF('./test/pov.dem')
 
 let { TestStream } = require('./TestStream')
 let messageHandler = require('../messages')
@@ -10,3 +10,11 @@ for (let message of demo.iterMessages()) {
   if (outputStream.index !== 0) outputStream.writeInt8(message.type)
   messageHandler.get(message.type).encode(outputStream, message)
 }
+
+let A = outputStream.buffer.slice(0, outputStream.index / 8)
+let B = demo.stream.buffer
+
+let AB = Buffer.compare(A, B)
+console.log('DECODE', B)
+console.log('ENCODE', A)
+console.log(AB)
